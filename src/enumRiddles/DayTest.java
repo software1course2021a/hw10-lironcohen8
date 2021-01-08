dbf99444e3618c49d8b881cfc20c6091648d61fb
lironcohen8 <lironhen12123@gmail.com>
2021-01-08 08:58:40 +0200

    riddle 2

--- src/enumRiddles/DayTest.java
+++ src/enumRiddles/DayTest.java
@@ -1,18 +1,26 @@
 package enumRiddles;
 
 enum Day {
-	   MONDAY,
-	   TUESDAY,
-	   WEDNESDAY,
-	   THURSDAY,
-	   FRIDAY,
-	   SATURDAY,
-	   SUNDAY;
+	   MONDAY(1),
+	   TUESDAY(2),
+	   WEDNESDAY(3),
+	   THURSDAY(4),
+	   FRIDAY(5),
+	   SATURDAY(6),
+	   SUNDAY(7);
+	
+	private final int num;
+	
+	private Day(int n) {
+		this.num = n;
+	}
 	 
-	   public Day next(){return MONDAY;}
+	   public Day next(){
+		   return Day.values()[this.num%Day.values().length];
+		   }
 	 
 	   int getDayNumber() {
-	      return 1;
+	      return this.num;
 	   }
 	}
 	   
